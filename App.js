997e6670993e20ba0f8f20eb27e24bca078dca89
Mamadou Moustapha Diallo <moustaphaotf@gmail.com>
2022-07-24 10:41:09 +0000

    Loading fonts

--- App.js
+++ App.js
@@ -1,13 +1,42 @@
 import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import * as SplashScreen from 'expo-splash-screen';
+import { useEffect, useState } from 'react';
+
+SplashScreen.preventAutoHideAsync();
 
 export default function App() {
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  useEffect(() => {
+    const prepare = async () => {
+      try {
+        await Font.loadAsync({
+          'pacifico': require('./assets/fonts/Pacifico-Regular.ttf'),
+          'nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+          'nunito-bold': require('./assets/fonts/Nunito-Bold.ttf'),
+        })
+      } catch(e) {
+        console.warn(e);
+      } finally {
+        setFontLoaded(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    fontLoaded ? (
+      <View style={styles.container}>
+        <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+        <StatusBar style="auto" />
+      </View>
+    ) : (
+      null
+    )
   );
 }
 
@@ -18,4 +47,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+    fontFamily: 'pacifico',
+    fontSize: 30,
+    textAlign: 'center'
+  }
 });
